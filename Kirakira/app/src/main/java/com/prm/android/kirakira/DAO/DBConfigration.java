6a75cf35222fcbbac334fb84f2d2d0c9a60a1932
kazypos <kazypos@gmail.com>
2017-07-05 16:43:19 +0700

    #Do: Create form project

---
+++ Kirakira/app/src/main/java/com/prm/android/kirakira/DAO/DBConfigration.java
@@ -0,0 +1,8 @@
+package com.prm.android.kirakira.DAO;
+
+/**
+ * Created by kazy on 7/5/2017.
+ */
+
+public class DBConfigration {
+}
---
+++ Kirakira/app/src/main/java/com/prm/android/kirakira/Utility/TextSpeechUtil.java
@@ -0,0 +1,8 @@
+package com.prm.android.kirakira.Utility;
+
+/**
+ * Created by kazy on 7/5/2017.
+ */
+
+public class TextSpeechUtil {
+}
