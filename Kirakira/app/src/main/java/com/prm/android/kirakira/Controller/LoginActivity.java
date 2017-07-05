23f1f8243ee38e4492bd7ca8e6ab33f2c47dffd2
kazypos <kazypos@gmail.com>
2017-07-05 16:38:35 +0700

    Create start-up project

--- Kirakira/app/src/main/java/com/prm/android/kirakira/Controller/LoginActivity.java
+++ Kirakira/app/src/main/java/com/prm/android/kirakira/Controller/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.prm.android.kirakira;
+package com.prm.android.kirakira.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.prm.android.kirakira.R;
 
 public class LoginActivity extends AppCompatActivity {
 
