d483d2f86b4f00968c06eb41f249b182cd0e91f7
ThinhDn <ngocthinh223@gmail.com>
2017-07-10 00:45:03 +0700

    Launch Screen & Icon app
    Dang bi loi cho doan chuyen man hinh tu launch_screen sang activity_login, ae ai biet cho nay bi gi fix ho phat

---
+++ Kirakira/app/src/main/java/com/prm/android/kirakira/Controller/LauncherActivity.java
@@ -0,0 +1,31 @@
+package com.prm.android.kirakira.Controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.prm.android.kirakira.R;
+
+/**
+ * Created by KING on 7/9/2017.
+ */
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launcher_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LauncherActivity.this, LoginActivity.class);
+                startActivity(i);
+                //finish();
+            }
+        },5000);
+    }
+}
